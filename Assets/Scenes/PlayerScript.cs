34d82f4e2e3c025b342c23e2faf738ca54370c0a
Mabe0116 <115063548+Mabe0116@users.noreply.github.com>
2024-08-11 22:07:06 +0900

    開始

--- Assets/Scenes/PlayerScript.cs
+++ Assets/Scenes/PlayerScript.cs
@@ -7,7 +7,7 @@ public class PlayerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Å@
     }
 
     // Update is called once per frame
